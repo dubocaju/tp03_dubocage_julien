f668903bc92f8ca3db48d320a7defc2759e17f3d
Julien Dubocage <dubocage.julien68@gmail.com>
2022-10-28 17:17:01 +0200

    fix: corrige le nom de l'app angular pour le déploiement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tp02_dubocage_julien';
+  title = 'tp03_dubocage_julien';
 }
